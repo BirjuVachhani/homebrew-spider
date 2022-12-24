3b9a7a9d32ca2b9e0902e308038d60bf2139e56e
BirjuVachhani <BirjuVachhani@users.noreply.github.com>
2022-12-24 17:34:31 +0000

    Bump up formula version to

--- Formula/spider.rb
+++ Formula/spider.rb
@@ -1,24 +1,22 @@
 class Spider < Formula
+  version ""
   desc "A small dart library to generate Assets dart code from assets folder."
   homepage "https://github.com/BirjuVachhani/spider"
-  url "https://github.com/BirjuVachhani/spider/archive/4.2.0.tar.gz"
-  sha256 "24d1bd138d2c6c085be1f3d77c0594aafb2db632f6a93b91518ba0127bb07de3"
   license "Apache-2.0"
-  
-#   bottle :unneeded
-  
-  depends_on "dart-lang/dart/dart" => :build
-  
+
+  if OS.mac?
+    url "https://github.com/BirjuVachhani/spider/releases/download/#{version}/spider-#{version}-macos.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  elsif OS.linux?
+    url "https://github.com/BirjuVachhani/spider/releases/download/#{version}/spider-#{version}-linux-amd64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  end
+
   def install
-    system "dart", "pub", "get"
-    system "dart", "compile", "exe", "bin/main.dart", "-o", "spider"
     bin.install "spider"
   end
-  
+
   test do
-    system "#{bin}/spider", "--version"
-    system "touch", "pubspec.yaml"
-    system "#{bin}/spider", "create"
-    raise 'test failed' unless File.exists? 'spider.yaml'
+    system "#{bin}/spider", "--help"
   end
 end
