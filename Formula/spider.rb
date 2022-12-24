263822d5d1ac8ca39f1a2ab424139d6c7bbdad3b
BirjuVachhani <BirjuVachhani@users.noreply.github.com>
2022-12-24 18:22:02 +0000

    Bump up formula version to 4.1.0

--- Formula/spider.rb
+++ Formula/spider.rb
@@ -1,15 +1,15 @@
 class Spider < Formula
-  version "4.2.0"
+  version "4.1.0"
   desc "A small dart library to generate Assets dart code from assets folder."
   homepage "https://github.com/BirjuVachhani/spider"
   license "Apache-2.0"
 
   if OS.mac?
     url "https://github.com/BirjuVachhani/spider/releases/download/#{version}/spider-#{version}-macos.tar.gz"
-    sha256 "38ee4122a35f17bba8a8a70cb0094726f6182784470d85c2f045664c6a650a22"
+    sha256 "38fcf83dd99c02df2384b73a3c22dbbb4ff674ddd2f0b3eb1e77b4159cf542ab"
   elsif OS.linux?
     url "https://github.com/BirjuVachhani/spider/releases/download/#{version}/spider-#{version}-linux-amd64.tar.gz"
-    sha256 "9cb08382c5d448ea2b556f6ac1852763a11c76953aa7d577806c06d42f783b6c"
+    sha256 "298767fd60285a73257e8396d42ea306b8db833b925236aa8d7ec43a58aaac5f"
   end
 
   def install
